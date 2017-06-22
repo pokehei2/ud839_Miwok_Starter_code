d536130df67a434f8b710b6c5a5ff981c7f81dea
pokehei2 <pokehei2@yahoo.com.hk>
2017-06-22 15:03:42 +0800

    Tab layout added, end of lesson 5 -verson 1.0
    End of the "Android Basics: Multiscreen Apps" course in lesson 5

--- app/src/main/java/com/example/android/miwok/CategoryAdapter.java
+++ app/src/main/java/com/example/android/miwok/CategoryAdapter.java
@@ -1,10 +1,13 @@
 package com.example.android.miwok;
 
+import android.content.Context;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentPagerAdapter;
 
 public class CategoryAdapter extends FragmentPagerAdapter {
+    private String tabTitles[] = new String[]{"Numbers", "Family", "Colors", "Phrases"};
+    private Context context;
 
     public CategoryAdapter(FragmentManager fm) {
         super(fm);
@@ -26,5 +29,11 @@ public class CategoryAdapter extends FragmentPagerAdapter {
     @Override
     public int getCount() {
         return 4;
+    }
+
+    @Override
+    public CharSequence getPageTitle(int position) {
+        // Generate title based on item position
+        return tabTitles[position];
     }
 }
